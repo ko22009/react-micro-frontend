10af89195329939710ee992984b39112951100ed
ko2 <ko22009@yandex.ru>
2023-01-04 13:47:18 +0500

    =init

---
+++ host/webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { dependencies } = require("./package.json");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "production",
+  devtool: false,
+  devServer: {
+    port: 3000,
+  },
+  optimization: {
+    minimize: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "Host",
+      remotes: {
+        Remote: `Remote@http://localhost:4000/moduleEntry.js`,
+      },
+      shared: {
+        ...dependencies,
+        react: {
+          singleton: true,
+          requiredVersion: dependencies["react"],
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: dependencies["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  target: "web",
+};
